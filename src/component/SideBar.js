c3d10340cf1cdb1dd791bbf8bd66f0d1eb156458
arimuhamad97 <kedemok98@gmail.com>
2022-12-21 00:12:26 +0800

    setup project

---
+++ src/component/SideBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Logo from "../utility/Logo";
+
+function SideBar() {
+  return (
+    <div className="flex flex-col pl-6 pt-2">
+      <Logo />
+      <div>
+        <p>Username</p>
+        <p>Akun/Admin</p>
+      </div>
+
+      <p>dashboard</p>
+      <p>dashboard</p>
+      <p>dashboard</p>
+      <p>dashboard</p>
+      <p>dashboard</p>
+      <p>dashboard</p>
+      <p>dashboard</p>
+      <p>dashboard</p>
+      <p>dashboard</p>
+    </div>
+  );
+}
+
+export default SideBar;
---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "../../component/SideBar";
+
+function Admin() {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+}
+
+export default Admin;
